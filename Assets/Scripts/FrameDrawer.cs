b5635a291e22d14bd691bd9fd1e8cac12f9719e7
gamesketches <sam.von.ehren@gmail.com>
2018-08-09 00:27:37 -0400

    Working on property drawer for frames

--- Assets/Scripts/FrameDrawer.cs
+++ Assets/Scripts/FrameDrawer.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using UnityEditor;
 
-//[CustomPropertyDrawer(typeof(Frame))]
+[CustomPropertyDrawer(typeof(Frame))]
 public class FrameDrawer : PropertyDrawer {
 
 	int heightSize = 4;
+	int spriteSize = 60;
 	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
 		EditorGUI.BeginProperty(position, label, property);
 		
@@ -12,15 +13,41 @@ public class FrameDrawer : PropertyDrawer {
 
 		var indent = EditorGUI.indentLevel;
 		EditorGUI.indentLevel = 0;
-		
-		Rect hitBoxRect = new Rect(position.x + 100, position.y, 50, position.height);
-		Rect hurtBoxRect = new Rect(position.x + 100, position.y + 30, 50, position.height);
-		Rect spriteRect = new Rect(position.x, position.y, 100, position.height);
 
-		GUIContent spriteImage = new GUIContent(property.FindPropertyRelative("sprite").objectReferenceValue as Texture2D);
-		EditorGUI.ObjectField(spriteRect, property.FindPropertyRelative("sprite"), spriteImage);
-		EditorGUI.PropertyField(hitBoxRect, property.FindPropertyRelative("hitBoxes"));
-		EditorGUI.PropertyField(hurtBoxRect, property.FindPropertyRelative("hurtBoxes"));
+		SerializedProperty spriteProp = property.FindPropertyRelative("sprite");
+		SerializedProperty hitBoxProp = property.FindPropertyRelative("hitBoxes");
+		SerializedProperty hurtBoxProp = property.FindPropertyRelative("hurtBoxes");
+
+		Sprite spriteTexture = (Sprite)spriteProp.objectReferenceValue;
+
+		int hitBoxArraySize = hitBoxProp.arraySize;
+
+		Rect hitBoxFieldRect = new Rect(position.x + 50, position.y, 16, 16);
+
+		EditorGUI.BeginChangeCheck();
+		int newSize = EditorGUI.IntField(hitBoxFieldRect, "HitBoxes", hitBoxArraySize);
+		if(newSize < 0) newSize = 0;
+		if(EditorGUI.EndChangeCheck()){
+			hitBoxProp.arraySize = newSize;
+			hitBoxArraySize = newSize;
+		}
+		
+		Rect hitBoxRect = new Rect(position.x + 50, position.y + 32, 64, 16);
+		Rect hurtBoxRect = new Rect(position.x + 50, position.y + 30, 50, position.height);
+		Rect spriteRect = new Rect(position.x - 100, position.y, 100, position.height);
+
+		EditorGUI.DrawTextureTransparent(spriteRect, spriteTexture.texture, ScaleMode.ScaleToFit);
+		//EditorGUI.PropertyField(hitBoxRect, property.FindPropertyRelative("hitBoxes"));
+		for(int i = 0; i< hitBoxArraySize; i++) {
+			Rect hitBox = (Rect)hitBoxProp.GetArrayElementAtIndex(i).rectValue;
+			EditorGUI.RectField(hitBoxRect, hitBox);
+			hitBox.xMin = spriteRect.xMax;
+			hitBox.yMin = spriteRect.yMax;
+			EditorGUI.DrawRect(hitBox, new Color(1f, 0f, 0f, 0.3f));
+			hitBoxRect = new Rect(hitBoxRect.xMin, hitBoxRect.yMin + 32, 128, heightSize);
+		}
+		if(hitBoxArraySize == 0) EditorGUI.DrawRect(spriteRect, Color.blue);
+		//EditorGUI.PropertyField(hurtBoxRect, property.FindPropertyRelative("hurtBoxes"));
 
 		EditorGUI.indentLevel = indent;
 
@@ -29,6 +56,6 @@ public class FrameDrawer : PropertyDrawer {
 
 	public override float GetPropertyHeight (SerializedProperty property, GUIContent label) {
 		
-		return base.GetPropertyHeight (property, label) * heightSize;
+		return base.GetPropertyHeight (property, label) * 8;
 	}
 }
